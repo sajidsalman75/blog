5b4014579272ab549cf0c662a61707fcdb7f9235
Salman Sajid <sajidsalman75@gmail.com>
2024-03-01 06:44:29 +0500

    initial commit

---
+++ client/src/PostCreate.js
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import axios from "axios";
+
+function PostCreate() {
+  const [title, setTitle] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    await axios.post("http://localhost:4000/posts", {
+      title,
+    });
+
+    setTitle("");
+  };
+  return (
+    <>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label>Title</label>
+            <input
+              className="form-control"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+          </div>
+          <button className="btn btn-primary">Submit</button>
+        </form>
+      </div>
+    </>
+  );
+}
+
+export default PostCreate;
